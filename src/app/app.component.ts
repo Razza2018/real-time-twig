a41b9c3e589e0fc7bf01ae1c098fdbefbf9ccc6c
Rohan Hope <rohanhope35@gmail.com>
2023-06-10 22:13:05 +1000

    Add dark mode feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+
   selectedPanel: string = 'html-twig';
 
   title = 'real-time-twig';
+
+  @ViewChild('siteWrapper') siteWrapper: any;
+
+  ngAfterViewInit(): void {
+    var darkModeOverride = null;
+
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+    darkModeOverride = localStorage.getItem('dark-mode');
+
+    if (darkModeOverride === 'true') {
+      this.toggleDarkMode();
+    }
+
+    if (darkModeOverride === null && prefersDarkScheme.matches) {
+      this.toggleDarkMode();
+    }
+  }
+
+  toggleDarkMode(): void {
+    var darkMode = false;
+
+    this.siteWrapper.nativeElement.classList.toggle('darkMode');
+
+    if (this.siteWrapper.nativeElement.classList.contains('darkMode')) {
+      darkMode = true;
+    }
+
+    localStorage.setItem('dark-mode', `${darkMode}`);
+  }
 
 }
